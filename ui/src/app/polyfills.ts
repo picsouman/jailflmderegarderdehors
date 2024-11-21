97844eff76180d51a7a126de77dc172d7fc956d0
Aimeric DELSEAUX <aimeric.01@hotmail.fr>
2024-11-21 16:32:58 +0100

    ajout des polyfills pour anciens navigateurs (a tester si ca fonctionne pour le chromium du raspberry)

---
+++ ui/src/app/polyfills.ts
@@ -0,0 +1,13 @@
+import 'zone.js'
+import 'whatwg-fetch'
+import 'intersection-observer'
+
+// intl
+import 'intl'
+import 'intl/locale-data/jsonp/en'
+
+// core-js
+import 'core-js/es/map'
+import 'core-js/es/set'
+import 'core-js/es/array'
+import 'core-js/es/promise'
